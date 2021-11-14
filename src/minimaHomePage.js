1c31bb172fbf44fd63bfb2fb88b67ef5940f7eda
harvinder52 <harvindergeek@gmail.com>
2021-11-14 12:09:15 +0530

    added nav,footer logos and text, added Manrope font, seperated out css file, added basic javascript for button to switch pages

---
+++ src/minimaHomePage.js
@@ -0,0 +1,25 @@
+console.log("made with love")
+document.querySelector("#extCheckPage").style.display = "none";
+document.querySelector(".flex-container").style.display = "none";
+
+
+let mainWrapper = document.querySelector(".mainWrapper");
+
+
+document.querySelector("#btnTry").addEventListener("click", ()=>{
+	
+	mainWrapper.style.display = "none";
+	document.querySelector("#extCheckPage").style.display = "initial";
+	console.log("button clicked: btnTry")
+});
+
+
+document.querySelector("#cardBtn").addEventListener("click", ()=>{
+	
+	
+	document.querySelector("#extCheckPage").style.display = "none";
+	document.querySelector(".flex-container").style.display = "initial";
+	console.log("button clicked: cardBtn")
+});
+
+
