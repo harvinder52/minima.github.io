ebec245055b79e642782b1da7f3a0a5a588c17c8
harvinder52 <harvindergeek@gmail.com>
2021-11-24 15:43:35 +0530

    email styling and some updates

--- src/minimaHomePage.js
+++ src/minimaHomePage.js
@@ -1,34 +1,44 @@
 console.log("made with love")
-// document.querySelector("#extCheckPage").style.display = "none";
-// document.querySelector(".flex-container").style.display = "none";
+
+
+let inputEmail = document.querySelector('#inputEmail');
+
+inputEmail.oninput = function (){
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputEmail.value))
+  {
+    console.log("email valid")
+    return (true)
+  }
+    console.log("You have entered an invalid email address!")
+    return (false)
+}
 
 
 let mainWrapper = document.querySelector(".mainWrapper");
 
 
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("emailBtn");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
   console.log("button clicked emalBtn")
+  document.body.style.overflow = "hidden"
 }
 
-// When the user clicks on <span> (x), close the modal
+
 span.onclick = function() {
   modal.style.display = "none";
+  document.body.style.overflow = "auto"
 }
 
-// When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+   document.body.style.overflow = "auto"
   }
 }
 
@@ -38,7 +48,7 @@ const form = document.forms['google-sheet']
 form.addEventListener('submit', e=>{
 	e.preventDefault()
 	fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-	.then(response => alert ("You have successfully subscribed for email reminder"))
+	.then(response => alert("You have successfully subscribed for email reminder"))
 	.catch(error => console.error('Error!', error.message))
 })
 
